3bfad5d05e04e0232d38fdf99ef8470bb2e15290
Mantvydas Liobė <mantvydasliobe@gmail.com>
2023-11-10 20:34:04 +0200

    features section

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,14 @@ import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeTo
 import { Header } from '@/components/Header/Header';
 import { Footer } from '@/components/Footer/Footer';
 import HeroSection from '@/components/HeroSection/HeroSection';
+import Features from '@/components/Features/Features';
 
 export default function HomePage() {
   return (
     <>
       <Header />
       <HeroSection />
+      <Features />
       <Welcome />
       <ColorSchemeToggle />
       <Footer />
