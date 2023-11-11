40ec0d190eac0aa8672f798d726a314684fbbdc7
Mantvydas Liobė <mantvydasliobe@gmail.com>
2023-11-11 17:14:53 +0200

    linting, prettier, text to maps folder

--- components/AboutUs/AboutUs.tsx
+++ components/AboutUs/AboutUs.tsx
@@ -1,79 +1,66 @@
-import { Container, Title, Text, Box } from "@mantine/core";
+import { Container, Title, Text, Box } from '@mantine/core';
 
-import { useLanguageContext } from "@/context/LanguageContext";
-import classes from "./AboutUs.module.css";
-import * as ltTranslation from "@/dictionaries/about/lt.json"
-import * as enTranslation from "@/dictionaries/about/en.json"
+import { useLanguageContext } from '@/context/LanguageContext';
+import classes from './AboutUs.module.css';
+import * as ltTranslation from '@/dictionaries/about/lt.json';
+import * as enTranslation from '@/dictionaries/about/en.json';
 
 // text paragraph item
 type propType = {
-    firstText: string,
-    secondText: string,
-    totenText: string,
-    code: string,
-    vat: string,
-    address: string,
-    phone: string,
-    email: string
-}
-const paragraphItem = (text : propType) => {
-   return (
-    <>
-         <Box>
-            <Text>{text.firstText}</Text>
-            <Text>{text.totenText}</Text>
-            <Text fw={500} py="lg">{text.secondText}</Text>
-        </Box>
+  firstText: string;
+  secondText: string;
+  totenText: string;
+  code: string;
+  vat: string;
+  address: string;
+  phone: string;
+  email: string;
+};
 
-        <Box mt="lg">       
-            <Text>
-            {text.code} -{" "}
-            <b>110842421</b>
-            </Text>
+const paragraphItem = (text: propType) => (
+  <>
+    <Box>
+      <Text>{text.firstText}</Text>
+      <Text>{text.totenText}</Text>
+      <Text fw={500} py="lg">
+        {text.secondText}
+      </Text>
+    </Box>
 
-            <Text>
-            {text.vat} -{" "}
-            <b>LT108424219</b>
-            </Text>
+    <Box mt="lg">
+      <Text>
+        {text.code} - <b>110842421</b>
+      </Text>
 
-            <Text>
-            {text.address} -{" "}
-            <b>A.Juozapavičiaus pr 3B, Kaunas</b>
-            </Text>
+      <Text>
+        {text.vat} - <b>LT108424219</b>
+      </Text>
 
-            <Text>
-            {text.phone}{" "}
-            - <b>+370 37 302375</b>
-            </Text>
+      <Text>
+        {text.address} - <b>A.Juozapavičiaus pr 3B, Kaunas</b>
+      </Text>
 
-            <Text>
-            {text.email} -{" "}
-            <b>info@toten-transport.lt</b>
-            </Text>
-        </Box>
-    </>
-)
-}
+      <Text>
+        {text.phone} - <b>+370 37 302375</b>
+      </Text>
+
+      <Text>
+        {text.email} - <b>info@toten-transport.lt</b>
+      </Text>
+    </Box>
+  </>
+);
 
 const AboutUs = () => {
   const { selectedLanguage } = useLanguageContext();
 
   return (
-    <Container
-      size={"md"}
-      className={classes.container}
-    >
-      <Title
-        order={1}
-        ta="center"
-        className="toten-red"
-        mb="lg"
-      >
+    <Container size="md" className={classes.container}>
+      <Title order={1} ta="center" className="toten-red" mb="lg">
         Toten Transport Lithuania, UAB
       </Title>
 
-      {selectedLanguage === "ltu" ? paragraphItem(ltTranslation) : paragraphItem(enTranslation)}
-
+      {selectedLanguage === 'ltu' ? paragraphItem(ltTranslation) : paragraphItem(enTranslation)}
     </Container>
   );
 };
--- components/GoogleMapsIframe/GoogleMapsIframe.tsx
+++ components/GoogleMapsIframe/GoogleMapsIframe.tsx
@@ -1,46 +1,40 @@
-import { SimpleGrid, Container, Center, Text } from "@mantine/core";
+import { SimpleGrid, Container, Center, Text } from '@mantine/core';
 
-import { useLanguageContext } from "@/context/LanguageContext";
-import classes from "./GoogleMapsIframe.module.css";
+import { useLanguageContext } from '@/context/LanguageContext';
+import classes from './GoogleMapsIframe.module.css';
+import * as ltTranslation from '@/dictionaries/maps/lt.json';
+import * as enTranslation from '@/dictionaries/maps/en.json';
+
+// single item
+type singeItemProps = {
+  title: string;
+  mapPlace: string;
+  iframeTitle: string;
+};
+const SingleItem = (data: singeItemProps) => (
+  <Center style={{ flexDirection: 'column' }}>
+    <Text size="lg" fw="500">
+      {data.title}
+    </Text>
+    <iframe
+      src={data.mapPlace}
+      className={classes.gIframe}
+      title={data.iframeTitle}
+      aria-hidden="false"
+      loading="lazy"
+    />
+  </Center>
+);
 
 const GoogleMapsIframe = () => {
   const { selectedLanguage } = useLanguageContext();
 
   return (
     <Container size="sm" pb="lg">
-      <SimpleGrid
-        spacing="lg"
-        cols={{ base: 1, sm: 2 }}
-      >
-        <Center style={{ flexDirection: "column" }}>
-          <Text size="lg" fw="500">
-            {selectedLanguage === "ltu" ? `Kontoros adresas` : "Office address"}
-          </Text>
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2296.353475035389!2d23.94600481577211!3d54.861604080324454!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46e7220e526e5937%3A0xc073b77d54d88e8!2sNordcarrier%20Baltic!5e0!3m2!1slt!2slt!4v1663436199566!5m2!1slt!2slt"
-            title="Toten transport Lithuania office address"
-            className={classes.gIframe}
-            aria-hidden="false"
-            tabIndex={0}
-            loading="lazy"
-          />
-        </Center>
-
-        <Center style={{ flexDirection: "column" }}>
-          <Text size="lg" fw="500">
-            {selectedLanguage === "ltu"
-              ? `Sandėlio adresas`
-              : "Warehouse address"}
-          </Text>
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2292.804175290485!2d24.064754515774652!3d54.92390838033981!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46e7177a6bf24acb%3A0x34339fb09aefa1df!2sNordcarrier%20Baltic%2C%20sand%C4%97lis!5e0!3m2!1slt!2slt!4v1663436314093!5m2!1slt!2slt"
-            title="Toten transport Lithuania warehouse address"
-            className={classes.gIframe}
-            aria-hidden="false"
-            tabIndex={0}
-            loading="lazy"
-          />
-        </Center>
+      <SimpleGrid spacing="lg" cols={{ base: 1, sm: 2 }}>
+        {selectedLanguage === 'ltu'
+          ? ltTranslation.map((place) => SingleItem(place))
+          : enTranslation.map((place) => SingleItem(place))}
       </SimpleGrid>
     </Container>
   );
