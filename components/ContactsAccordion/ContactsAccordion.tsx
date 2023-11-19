8d45752f6c92670650600f2ac864d4106064ca57
Mantvydas Liobė <mantvydasliobe@gmail.com>
2023-11-19 19:29:56 +0200

    contacts accordion using i18n translations

--- components/ContactsAccordion/ContactsAccordion.tsx
+++ components/ContactsAccordion/ContactsAccordion.tsx
@@ -6,77 +6,57 @@ import classes from './ContactsAccordion.module.css';
 
 const totenColor = '#E31937';
 
-// accordion item
-type AccordionItemProps = {
-  id: string;
-  name: string;
-  person: string;
-  mobHref: string;
-  mob: string;
-  mailHref: string;
-  mail: string;
-  mailHref2?: string;
-  mail2?: string;
-};
-const AccordionItem = ({
-  id,
-  name,
-  person,
-  mobHref,
-  mob,
-  mailHref,
-  mail,
-  mailHref2 = '',
-  mail2 = '',
-}: AccordionItemProps) => (
-  <Accordion.Item value={id} key={id}>
-    <Accordion.Control>{name}</Accordion.Control>
-    <Accordion.Panel>
-      <Box ta="center">
-        <Stack justify="center" gap="xs">
-          <Group justify="center">
-            <IconUser size="24" color={totenColor} />
-            <Text tt="uppercase" fw={500}>
-              {person}
-            </Text>
-          </Group>
-
-          <Group justify="center" gap="xs">
-            <IconPhoneCall size="16" color={totenColor} />
-            <a href={`tel:${mobHref}`}>{`Mob.${mob}`}</a>
-            <br />
-          </Group>
-
-          <Group justify="center" gap="xs">
-            <IconMail size="16" color={totenColor} />
-            <a href={`mailto:${mailHref}`}>{mail}</a>
-          </Group>
-
-          {mail2 && (
-            <Group justify="center" gap="xs">
-              <IconMail size="16" color={totenColor} />
-              <a href={`mailto:${mailHref2}`}>{mail2}</a>
-            </Group>
-          )}
-        </Stack>
-      </Box>
-    </Accordion.Panel>
-  </Accordion.Item>
-);
-
-type ContactsAccordionProps = {
-  text: AccordionItemProps[];
-};
-
-const ContactsAccordion = ({ text }: ContactsAccordionProps) =>{
-  const t = useTranslations('Contacts')
+const ContactsAccordion = () => {
+  const translationKey = 'Contacts'
+  const t = useTranslations(translationKey)
+  const contactsKeys = ["INo", "IsNo", "Muit", "Kiti", "Fin", "Buh", "Rel", "Sand", "Vad"];
 
 return (
   <Container size="sm" className={classes.accordionContainer}>
     <Box ta="center">
       <IconPhoneCall size="32" color="green" />
     </Box>
-    <Accordion>{text && text.map((item: AccordionItemProps) => AccordionItem(item))}</Accordion>
+    <Accordion>
+      {contactsKeys.map((contact) => {
+        // if no translation next-intl returns a ${namespace}.${key} string. 
+        const hasOptionalMail = t(`${contact}.mail2`) !== `${translationKey}.${contact}.mail2`;
+
+      return (
+            <Accordion.Item value={t(`${contact}.id`)} key={t(`${contact}.id`)}>
+              <Accordion.Control>{t(`${contact}.name`)}</Accordion.Control>
+              <Accordion.Panel>
+                <Box ta="center">
+                  <Stack justify="center" gap="xs">
+                    <Group justify="center">
+                      <IconUser size="24" color={totenColor} />
+                      <Text tt="uppercase" fw={500}>
+                        {t(`${contact}.person`)}
+                      </Text>
+                    </Group>
+          
+                    <Group justify="center" gap="xs">
+                      <IconPhoneCall size="16" color={totenColor} />
+                      <a href={`tel:${t(`${contact}.mobHref`)}`}>{`Mob.${t(`${contact}.mobHref`)}`}</a>
+                      <br />
+                    </Group>
+          
+                    <Group justify="center" gap="xs">
+                      <IconMail size="16" color={totenColor} />
+                      <a href={`mailto:${t(`${contact}.mailHref`)}`}>{t(`${contact}.mail`)}</a>
+                    </Group>
+          
+                    {hasOptionalMail && (
+                      <Group justify="center" gap="xs">
+                        <IconMail size="16" color={totenColor} />
+                        <a href={`mailto:${t(`${contact}.mailHref2`)}`}>{t(`${contact}.mail2`)}</a>
+                      </Group>
+                    )}
+                  </Stack>
+                </Box>
+              </Accordion.Panel>
+            </Accordion.Item>)}
+      )}
+    </Accordion>
   </Container>
 );}
 
