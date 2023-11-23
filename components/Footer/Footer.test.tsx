775e05b9f531ca7c2534d1d2adb099edfc2e784f
Mantvydas Liobė <mantvydasliobe@gmail.com>
2023-11-23 11:31:09 +0200

    footer tests

---
+++ components/Footer/Footer.test.tsx
@@ -0,0 +1,41 @@
+import { NextIntlClientProvider } from 'next-intl';
+import { render, screen, fireEvent } from '@/test-utils';
+import { Footer } from './Footer';
+import TranslationsENG from '@/messages/en.json';
+
+const component = () =>
+  render(
+    <NextIntlClientProvider locale="en" messages={TranslationsENG}>
+      <Footer />
+    </NextIntlClientProvider>
+  );
+
+describe('Footer tests', () => {
+  it('renders Footer with ENG text', () => {
+    component();
+    expect(
+      screen.getByText((content) => content.trim().includes(TranslationsENG.Footer.rights))
+    ).toBeInTheDocument();
+  });
+
+  it('renders links', () => {
+    component();
+    expect(screen.getByText(TranslationsENG.Footer.goLink.label)).toHaveAttribute(
+      'href',
+      TranslationsENG.Footer.goLink.link
+    );
+    expect(screen.getByText(TranslationsENG.Footer.goLink.label)).toHaveAttribute(
+      'href',
+      TranslationsENG.Footer.goLink.link
+    );
+  });
+
+  it('show working time when open hours pressed', () => {
+    component();
+    const button = screen.getByText(TranslationsENG.WorkingTime.working);
+
+    fireEvent.click(button);
+
+    expect(screen.getAllByText(TranslationsENG.WorkingTime.time)).toBeTruthy();
+  });
+});
