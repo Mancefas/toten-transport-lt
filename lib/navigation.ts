817861f253cff6b07d2ad0ba4d0063504c819c95
Mantvydas Liobė <mantvydasliobe@gmail.com>
2023-12-23 10:32:33 +0200

    feature: added NOR language

--- lib/navigation.ts
+++ lib/navigation.ts
@@ -1,8 +1,8 @@
 import { createSharedPathnamesNavigation } from 'next-intl/navigation';
 
-export const locales = ['lt', 'en'] as const;
+export const locales = ['lt', 'en', 'no'] as const;
 
-export type SupportedLocale = 'lt' | 'en';
+export type SupportedLocale = 'lt' | 'en' | 'no';
 
 export const { Link, redirect, usePathname, useRouter } = createSharedPathnamesNavigation({
   locales,
